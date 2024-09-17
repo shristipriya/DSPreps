3ba3c4086c36b052a2ef517141ea2ac4e97c519e
Shristi Priya <shristi.priya@sap.com>
2024-09-17 13:00:21 +0530

    linkedlist

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-//        LL list = new LL();
-//        list.insertFirst(4);
-//        list.insertFirst(2);
-//        list.insertFirst(3);
-//        list.insertFirst(0);
-//        list.insertFirst(7);
+        LL list = new LL();
+        list.insertFirst(4);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(0);
+        list.insertFirst(7);
 //        list.insert(44, 3);
 //        list.deleteFirst();
 //        list.deleteLast();
 //        list.delete(2);
-//        list.display();
+
+        list.insertRecursion(88, 2);
+        list.display();
 
 //        DLL list = new DLL();
 //
@@ -24,13 +26,13 @@ public class Main {
 //        list.display();
 
 
-        CLL list = new CLL();
-        list.insert(23);
-        list.insert(3);
-        list.insert(19);
-        list.insert(75);
-        list.display();
-        list.delete(19);
-        list.display();
+//        CLL list = new CLL();
+//        list.insert(23);
+//        list.insert(3);
+//        list.insert(19);
+//        list.insert(75);
+//        list.display();
+//        list.delete(19);
+//        list.display();
     }
 }
